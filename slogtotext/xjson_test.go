8a0ae4b29fd25fc31dfcf7ab8fbf3305019e414c
Alexey Michurin <a.michurin@gmail.com>
2024-11-16 08:19:59 +0300

    CI: golangci-lint: 1.62

--- slogtotext/xjson_test.go
+++ slogtotext/xjson_test.go
@@ -10,7 +10,7 @@ func TestTXJson(t *testing.T) {
 	t.Run("json", func(t *testing.T) {
 		data := `{"a": true, "b": null, "c": 1, "d": "xxx", "e": "[1,\"{\\\"p\\\":55}\",3]"}`
 		res := tXJson(data)
-		assert.Equal(t, `{"a":true,"b":null,"c":1,"d":"xxx","e":[1,{"p":55},3]}`, res)
+		assert.Equal(t, `{"a":true,"b":null,"c":1,"d":"xxx","e":[1,{"p":55},3]}`, res) //nolint:testifylint
 	})
 	t.Run("raw", func(t *testing.T) {
 		data := `xxx`
